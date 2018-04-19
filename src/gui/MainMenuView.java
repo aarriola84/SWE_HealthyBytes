6a63073c354769ce906ebd0c77e96d968d293ae1
luis gonzalez <luis.a.gonzalez06@utrgv.edu>
2018-04-18 22:47:24 -0500

    Added MainMenuView and images folder with image placeholder

---
+++ src/gui/MainMenuView.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+
+/**
+ *
+ * @author Luis G
+ */
+public class MainMenuView extends GridPane
+{
+    // Main menu labels and others
+    private Label search = new Label("Search: ");
+    private Label dishInfo = new Label("Dish Info: ");
+    private Label title = new Label("Healthy Bytes");
+
+    // Main menu TextFields and TextAreas
+    private TextField searchTF = new TextField();
+    private TextArea dishInfoTA = new TextArea();
+
+    //Main menu buttons
+    private Button searchBtn = new Button("Seach");
+    private Button myRecipeBtn = new Button("My Recipes");
+    private Button sendBtn = new Button("Send");
+    private Button printBtn = new Button("Print");
+    
+    //main menu images
+    private Image dishImage = new Image("utb.jpg");
+    private ImageView dishIV = new ImageView(dishImage);
+
+    public MainMenuView()
+    {
+        //format title label 
+        title.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.ITALIC, 20));
+        
+        //set gap for h and v boxes
+        this.setHgap(10);
+        this.setVgap(10);
+
+        //vbox variables
+        VBox vbox0 = new VBox();
+        VBox vbox1 = new VBox();
+        VBox vbox2 = new VBox();
+
+        //hbox variables
+        HBox hbox1 = new HBox();
+        HBox hbox2 = new HBox();
+
+        //setup for all labes, text fields, etc.
+        this.getChildren().clear();
+        vbox1.getChildren().addAll(title, dishIV, search, searchTF);
+        hbox1.getChildren().addAll(searchBtn, myRecipeBtn);
+        vbox2.getChildren().addAll(dishInfo, dishInfoTA);
+        hbox2.getChildren().addAll(sendBtn, printBtn);
+        
+        //outputting all GUIS here
+        this.add(vbox1, 1, 0);
+        this.add(hbox1, 1, 1);
+        this.add(vbox2, 1, 2);
+        this.add(hbox2, 1, 3);
+
+    }
+
+}
