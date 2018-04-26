a20cc1ed92dd8dae9ff3dadfedb8179639984328
Alexis Arriola <alexis.arriola01@utrgv.edu>
2018-04-25 19:11:46 -0500

    testing merges, made small edits to the git cheat sheet, and added the final DB

--- healthybites2.sql
+++ healthybites2.sql
@@ -16,27 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `ingredientlist`
+-- Table structure for table `faves`
 --
 
-DROP TABLE IF EXISTS `ingredientlist`;
+DROP TABLE IF EXISTS `faves`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ingredientlist` (
-  `ingr_list_id` int(11) NOT NULL AUTO_INCREMENT,
-  `ingr_list` tinytext NOT NULL,
-  PRIMARY KEY (`ingr_list_id`),
-  UNIQUE KEY `ingr_list_id_UNIQUE` (`ingr_list_id`)
+CREATE TABLE `faves` (
+  `_recID` int(11) NOT NULL,
+  `_userID` int(11) NOT NULL,
+  PRIMARY KEY (`_recID`,`_userID`),
+  KEY `_userID_idx` (`_userID`),
+  CONSTRAINT `_recID` FOREIGN KEY (`_recID`) REFERENCES `recipes` (`recipe_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `_userID` FOREIGN KEY (`_userID`) REFERENCES `recipes` (`ownership_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `ingredientlist`
+-- Dumping data for table `faves`
 --
 
-LOCK TABLES `ingredientlist` WRITE;
-/*!40000 ALTER TABLE `ingredientlist` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ingredientlist` ENABLE KEYS */;
+LOCK TABLES `faves` WRITE;
+/*!40000 ALTER TABLE `faves` DISABLE KEYS */;
+/*!40000 ALTER TABLE `faves` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -51,7 +53,7 @@ CREATE TABLE `proteintype` (
   `name` varchar(15) NOT NULL,
   PRIMARY KEY (`proType_id`),
   UNIQUE KEY `proType_id_UNIQUE` (`proType_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -60,7 +62,7 @@ CREATE TABLE `proteintype` (
 
 LOCK TABLES `proteintype` WRITE;
 /*!40000 ALTER TABLE `proteintype` DISABLE KEYS */;
-INSERT INTO `proteintype` VALUES (1,'Chicken'),(2,'Beef'),(3,'Fish'),(4,'Lamb'),(5,'Vegan'),(6,'Other');
+INSERT INTO `proteintype` VALUES (1,'Chicken'),(2,'Beef'),(3,'Fish'),(4,'Lamb'),(5,'Pork'),(6,'Vegan'),(7,'Vegetarian'),(8,'Other');
 /*!40000 ALTER TABLE `proteintype` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -76,22 +78,16 @@ CREATE TABLE `recipes` (
   `name` varchar(25) NOT NULL,
   `fave_count` int(11) DEFAULT NULL,
   `rec_img` blob,
-  `ownership_id` int(11) NOT NULL,
-  `steps_id` int(11) NOT NULL,
-  `ingr_list_id` int(11) NOT NULL,
+  `steps` mediumtext,
+  `ingr_list` mediumtext,
   `protein_id` int(11) NOT NULL,
+  `ownership_id` int(11) NOT NULL,
   PRIMARY KEY (`recipe_id`),
   UNIQUE KEY `recipe_id_UNIQUE` (`recipe_id`),
-  KEY `ingr_list_id_idx` (`ingr_list_id`),
-  KEY `steps_id_idx` (`steps_id`),
-  KEY `rec_steps_id_idx` (`steps_id`),
-  KEY `rec_ingr_list_id_idx` (`ingr_list_id`),
   KEY `protein_type_idx` (`protein_id`),
   KEY `ownership_id_idx` (`ownership_id`),
   CONSTRAINT `ownership_id` FOREIGN KEY (`ownership_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `protein_id` FOREIGN KEY (`protein_id`) REFERENCES `proteintype` (`proType_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `rec_ingr_list_id` FOREIGN KEY (`ingr_list_id`) REFERENCES `ingredientlist` (`ingr_list_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `rec_steps_id` FOREIGN KEY (`steps_id`) REFERENCES `steps` (`step_id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `protein_id` FOREIGN KEY (`protein_id`) REFERENCES `proteintype` (`proType_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -102,30 +98,6 @@ CREATE TABLE `recipes` (
 LOCK TABLES `recipes` WRITE;
 /*!40000 ALTER TABLE `recipes` DISABLE KEYS */;
 /*!40000 ALTER TABLE `recipes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `steps`
---
-
-DROP TABLE IF EXISTS `steps`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `steps` (
-  `step_id` int(11) NOT NULL AUTO_INCREMENT,
-  `steps` mediumtext NOT NULL,
-  PRIMARY KEY (`step_id`),
-  UNIQUE KEY `step_id_UNIQUE` (`step_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `steps`
---
-
-LOCK TABLES `steps` WRITE;
-/*!40000 ALTER TABLE `steps` DISABLE KEYS */;
-/*!40000 ALTER TABLE `steps` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -141,7 +113,8 @@ CREATE TABLE `users` (
   `password` varchar(35) NOT NULL,
   `fName` varchar(15) NOT NULL,
   `lName` varchar(35) NOT NULL,
-  `dob` date NOT NULL,
+  `dob` varchar(10) NOT NULL,
+  `authenticate` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_id_UNIQUE` (`user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
@@ -153,7 +126,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'kaybee@gmail.com','pewpew123','','','0000-00-00'),(2,'dagh123@gmail.com','powpow123','','','0000-00-00');
+INSERT INTO `users` VALUES (1,'kaybee@gmail.com','pewpew123','Kayla','Buentello','06-29-1992',1),(2,'dagh123@gmail.com','powpow123','Alex','Arriola','09-04-1989',1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -166,4 +139,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-24 19:33:30
+-- Dump completed on 2018-04-25 19:08:42
