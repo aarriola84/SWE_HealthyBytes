3aa916a38ccbb03504cfa8b45e1d7ee829d96e7e
Matthew Rodriguez <mattrodriguez64@gmail.com>
2018-04-25 21:20:58 -0500

    Added the new MyRecipesForm.java with the properties

--- src/mainmenu/MyRecipesForm.java
+++ src/mainmenu/MyRecipesForm.java
@@ -40,19 +40,149 @@ public class MyRecipesForm extends BorderPane
        new Label("This is a recipe about lasagna."),
     };
     
-    ListView<String> listview = new ListView<>(FXCollections.observableArrayList(foodTitles)); // ArrayList to hold things
-    FlowPane listpane = new FlowPane (10,10);
+    private ListView<String> listview = new ListView<>(FXCollections.observableArrayList(foodTitles)); // ArrayList to hold things
+    private FlowPane listpane = new FlowPane (10,10);
 
     private Label titleLabel = new Label("My Recipes");
     private Button viewrecipeBtn = new Button("View Recipe");
     private Button createrecipeBtn = new Button("Create Recipe");
     private Button shareBtn = new Button ("Share Button");
     
+    
+    /**
+     * @return the foodTitles
+     */
+    public String[] getFoodTitles()
+    {
+        return foodTitles;
+    }
+
+    /**
+     * @param foodTitles the foodTitles to set
+     */
+    public void setFoodTitles(String[] foodTitles)
+    {
+        this.foodTitles = foodTitles;
+    }
+
+    /**
+     * @return the foodDescription2
+     */
+    public Label[] getFoodDescription2()
+    {
+        return foodDescription2;
+    }
+
+    /**
+     * @param foodDescription2 the foodDescription2 to set
+     */
+    public void setFoodDescription2(Label[] foodDescription2)
+    {
+        this.foodDescription2 = foodDescription2;
+    }
+
+    /**
+     * @return the listview
+     */
+    public ListView<String> getListview()
+    {
+        return listview;
+    }
+
+    /**
+     * @param listview the listview to set
+     */
+    public void setListview(ListView<String> listview)
+    {
+        this.listview = listview;
+    }
+
+    /**
+     * @return the listpane
+     */
+    public FlowPane getListpane()
+    {
+        return listpane;
+    }
+
+    /**
+     * @param listpane the listpane to set
+     */
+    public void setListpane(FlowPane listpane)
+    {
+        this.listpane = listpane;
+    }
+
+    /**
+     * @return the titleLabel
+     */
+    public Label getTitleLabel()
+    {
+        return titleLabel;
+    }
+
+    /**
+     * @param titleLabel the titleLabel to set
+     */
+    public void setTitleLabel(Label titleLabel)
+    {
+        this.titleLabel = titleLabel;
+    }
+
+    /**
+     * @return the viewrecipeBtn
+     */
+    public Button getViewrecipeBtn()
+    {
+        return viewrecipeBtn;
+    }
+
+    /**
+     * @param viewrecipeBtn the viewrecipeBtn to set
+     */
+    public void setViewrecipeBtn(Button viewrecipeBtn)
+    {
+        this.viewrecipeBtn = viewrecipeBtn;
+    }
+
+    /**
+     * @return the createrecipeBtn
+     */
+    public Button getCreaterecipeBtn()
+    {
+        return createrecipeBtn;
+    }
+
+    /**
+     * @param createrecipeBtn the createrecipeBtn to set
+     */
+    public void setCreaterecipeBtn(Button createrecipeBtn)
+    {
+        this.createrecipeBtn = createrecipeBtn;
+    }
+
+    /**
+     * @return the shareBtn
+     */
+    public Button getShareBtn()
+    {
+        return shareBtn;
+    }
+
+    /**
+     * @param shareBtn the shareBtn to set
+     */
+    public void setShareBtn(Button shareBtn)
+    {
+        this.shareBtn = shareBtn;
+    }
+    
     public MyRecipesForm()
     {
         listview.setPrefSize(400,400);
         listview.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-        this.setLeft(new ScrollPane(listpane));
+        this.setTop(titleLabel);
+        this.setLeft(new ScrollPane(getListpane()));
         this.setCenter(listpane);
         
         listview.getSelectionModel().selectedItemProperty().addListener(
